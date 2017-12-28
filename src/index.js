e7e962fb3830d1c3f8f9511a875aa7155211ecf3
Kyle Kelley <rgbkrk@gmail.com>
2017-12-28 12:53:54 -0800

    make a consistently tradable setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // @flow
 // @format
 
-console.log('one day I hope immutable and flow are stable');
+console.log("one day I hope immutable and flow are stable");
 
-const Immutable = require('immutable');
+const Immutable = require("immutable");
 
-const config = Immutable.Map({
-  theme: 'light',
+let config: Immutable.Map<*, *>;
+
+config = Immutable.Map({
+  theme: "light"
 });
