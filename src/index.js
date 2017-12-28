2f246c27a316106a6bb91941f8e24657b961ce58
Kyle Kelley <rgbkrk@gmail.com>
2017-12-28 12:46:27 -0800

    updated package

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// @flow
+// @format
+
+console.log('one day I hope immutable and flow are stable');
+
+const Immutable = require('immutable');
+
+const config = Immutable.Map({
+  theme: 'light',
+});
